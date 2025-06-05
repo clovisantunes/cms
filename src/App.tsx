30e7a8a7bc67d31ddfbcdc149038227096a41426
unknown <clovissantannaa@gmail.com>
2025-06-05 12:31:58 -0300

    Fix colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { About } from "./components/About"
 import { BannerCarousel } from "./components/Banner"
 import { Cards } from "./components/Cards"
 import { CTA } from "./components/CTA"
-import Footer from "./components/Footer"
+import {Footer} from "./components/Footer"
 import { InfoCards } from "./components/InfoCards"
 import { MinhaSaude } from "./components/MinhaSaude"
 import Navbar from "./components/Navbar"
 import { StatsSection } from "./components/StateSection"
 
 function App() {
-
-
   return (
     <>
       <Navbar />
