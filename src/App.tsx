b4c3e1c3974c61a58cb7aa0bbf892a3ef1fcf7d6
Clovis Antunes <clovissantannaa@gmail.com>
2026-01-15 08:18:07 -0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { About } from "./components/About"
 import { BannerCarousel } from "./components/Banner"
-import { Cards } from "./components/Cards"
-import { CTA } from "./components/CTA"
 import { Footer } from "./components/Footer"
 import { InfoCards } from "./components/InfoCards"
 import { MinhaSaude } from "./components/MinhaSaude"
 import Navbar from "./components/Navbar"
-import { StatsSection } from "./components/StateSection"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ScrollToTop } from './Utils/ScrollToTop/ScrollToTop';
 import Institucional from "./pages/Institucional"
