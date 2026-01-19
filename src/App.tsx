cd2c8aca53defeaab50759f022253fa5b941cb0a
Clovis Antunes <clovissantannaa@gmail.com>
2026-01-19 10:57:14 -0300

    Fix nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import { About } from "./components/About"
-import { BannerCarousel } from "./components/Banner"
-import { Footer } from "./components/Footer"
-import { InfoCards } from "./components/InfoCards"
-import { MinhaSaude } from "./components/MinhaSaude"
-import Navbar from "./components/Navbar"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { About } from "./components/About";
+import { BannerCarousel } from "./components/Banner";
+import { Footer } from "./components/Footer";
+import { InfoCards } from "./components/InfoCards";
+import { MinhaSaude } from "./components/MinhaSaude";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ScrollToTop } from './Utils/ScrollToTop/ScrollToTop';
-import Institucional from "./pages/Institucional"
-import ParaVoce from "./pages/ParaVoce"
-import ParaSuaEmpresa from "./pages/ParaSuaEmpresa"
-import Contato from "./pages/Contato"
-import WhatsApp from "./components/UI/WhatssApp"
-import Especialidades from "./pages/Especialidades"
-import Procedimentos from "./pages/Procedimentos"
-import Odontologia from "./pages/Odontologia"
-import Exames from "./pages/Exames"
-import ExamResults from "./components/Exames"
-import Error404 from "./pages/Error404" 
-import FilterSpecialties from "./components/FilterSpecialties"
-import EspecialidadePage from "./pages/EspecialidadesPage"
-import CartaoAPF from "./components/APF"
-import FAQSection from "./components/FAQ"
-import ExamPreparations from "./pages/examPreparations"
+import Institucional from "./pages/Institucional";
+import ParaVoce from "./pages/ParaVoce";
+import ParaSuaEmpresa from "./pages/ParaSuaEmpresa";
+import Contato from "./pages/Contato";
+import WhatsApp from "./components/UI/WhatssApp";
+import Especialidades from "./pages/Especialidades";
+import Procedimentos from "./pages/Procedimentos";
+import Odontologia from "./pages/Odontologia";
+import Exames from "./pages/Exames";
+import Error404 from "./pages/Error404";
+import FilterSpecialties from "./components/FilterSpecialties";
+import EspecialidadePage from "./pages/EspecialidadesPage";
+import CartaoAPF from "./components/APF";
+import FAQSection from "./components/FAQ";
+import ExamPreparations from "./pages/examPreparations";
 
 function App() {
   return (
